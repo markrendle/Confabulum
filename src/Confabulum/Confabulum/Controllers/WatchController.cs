8d7aaefe7e7fed54636bc2e37257e50c7ce2238a
Mark Rendle <mark@markrendle.net>
2014-09-07 18:54:58 +0100

    Initial commit

---
+++ src/Confabulum/Confabulum/Controllers/WatchController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Confabulum.Controllers
+{
+    public class WatchController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index(string deck, string show)
+        {
+            return View();
+        }
+    }
+}
